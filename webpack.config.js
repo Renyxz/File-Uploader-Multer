86ef6c713c783b9e1c91bffe2955622f94c0b097
Ren <ren.yxz.dev@gmail.com>
2017-12-07 20:20:14 -0500

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'build')
+    },
+    // plugins: [
+    //     new htmlWebpackPlugin({
+            
+    //     })
+    // ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                loader: 'file-loader',
+                options: {
+                    outputPath: 'assets/img/'
+                }
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                loader: 'file-loader',
+                options: {
+                    outputPath: 'assets/fonts/'
+                }
+            }
+        ]
+    
+    }
+
+}
